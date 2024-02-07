a60e83bc42044d394477bedece74f14d4d8d98c9
HugoLaurent <lauhugopro@gmail.com>
2024-02-07 13:38:04 +0100

    Mise à jour de la page des planètes

--- script.js
+++ script.js
@@ -16,3 +16,62 @@ async function getStats() {
 }
 
 getStats();
+
+// PLANET PAGE JS
+
+async function getAllPlanets() {
+  const arrayPlanet = [];
+  for (let i = 1; i <= 6; i++) {
+    const planets = await fetch(`https://swapi.dev/api/planets/?page=${i}`);
+    const planetsData = await planets.json();
+    arrayPlanet.push(...planetsData.results);
+  }
+  return arrayPlanet;
+}
+
+const planetList = document.querySelector(".planet-list");
+const nbPlanet = document.querySelector(".nb-planet");
+const loader = document.querySelector(".loader-planet-detail");
+async function getPlanets() {
+  const allPlanet = await getAllPlanets();
+  console.log(allPlanet);
+
+  allPlanet.forEach((planet) => {
+    const planetItem = document.createElement("li");
+    planetItem.textContent = planet.name;
+    planetList.appendChild(planetItem);
+    const planetDetails = document.createElement("span");
+    planetDetails.textContent = planet.terrain;
+    planetItem.appendChild(planetDetails);
+    loader.style.display = "none";
+  });
+  nbPlanet.textContent = allPlanet.length + " planets found.";
+}
+
+getPlanets();
+
+// ON CGHANGE INPUT
+
+const input = document.querySelector(".search-planet");
+input.addEventListener("input", (e) => {
+  const value = e.target.value;
+  const planetName = document.querySelectorAll(".planet-list li");
+  planetName.forEach((planet) => {
+    if (planet.textContent.toLowerCase().includes(value.toLowerCase())) {
+      planet.style.display = "flex";
+    } else {
+      planet.style.display = "none";
+    }
+  });
+  const pluralOrNot =
+    document.querySelectorAll(".planet-list li:not([style='display: none;'])")
+      .length > 1
+      ? "s"
+      : "";
+  nbPlanet.textContent =
+    document.querySelectorAll(".planet-list li:not([style='display: none;'])")
+      .length +
+    " planet" +
+    pluralOrNot +
+    " found.";
+});
